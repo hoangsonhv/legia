381a3a03fa803fa0523443462d7853fb83c33f26
dattt <dattt@syn-gr.com>
2024-01-27 17:19:30 +0700

    update

--- resources/views/admins/pdf/warehouse-export.blade.php
+++ resources/views/admins/pdf/warehouse-export.blade.php
@@ -6,12 +6,12 @@
     <title>Lệnh sản xuất</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    {{-- <link rel="stylesheet" href={{asset('css/pdf/manufacture-check.css')}} /> --}}
+    <link rel="stylesheet" href={{asset('css/pdf/manufacture-check.css')}} />
 </head>
 
 <body>
     <div id="pdf-manufacture" style=" transform: rotate(-90deg);">
-        <table border="0" cellspacing="0" cellpadding="0" style="font-family: Arial, sans-serif;">
+        <table border="0" cellspacing="0" cellpadding="0" >
             <tr>
                 <td rowspan="5" style="padding-right: 10px;">
                     <img width="220px" height="100px" src="{{ asset('images/logo.jpg') }}" />
