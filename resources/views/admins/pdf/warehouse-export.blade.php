5ca81b207d104f8636c956a89be38d9d6eacde0d
Gin <gin.an@vand.vn>
2024-01-29 17:13:02 +0700

    update pdf

--- resources/views/admins/pdf/warehouse-export.blade.php
+++ resources/views/admins/pdf/warehouse-export.blade.php
@@ -6,7 +6,7 @@
     <title>Lệnh sản xuất</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    {{-- <link rel="stylesheet" href={{ asset('css/pdf/manufacture-check.css') }} /> --}}
+    <link rel="stylesheet" href={{ asset('css/pdf/manufacture-check.css') }} />
 </head>
 
 <body>
