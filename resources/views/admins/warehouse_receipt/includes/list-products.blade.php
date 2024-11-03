aee885562bb401a9b1aa354f2cb4f660763c3f61
SonHV <hvsson102@gmail.com>
2024-11-03 22:39:26 +0700

    Fix view edit warehouse-receipt

--- resources/views/admins/warehouse_receipt/includes/list-products.blade.php
+++ resources/views/admins/warehouse_receipt/includes/list-products.blade.php
@@ -21,7 +21,18 @@
         <th class="align-middle">L L1</th>
         <th class="align-middle">W2</th>
         <th class="align-middle">L2</th>
-        <th class="align-middle">Đ/v tính</th>
+          <th class="align-middle material_th_custom">Inner</th>
+          <th class="align-middle material_th_custom">Hoop</th>
+          <th class="align-middle material_th_custom">Filler</th>
+          <th class="align-middle material_th_custom">Outer</th>
+          <th class="align-middle material_th_custom">Thick</th>
+          <th class="align-middle material_th_custom">Tiêu chuẩn</th>
+          <th class="align-middle material_th_custom">K.Cỡ</th>
+          <th class="align-middle material_th_custom">Trọng lượng - Kg/Cuộn</th>
+          <th class="align-middle material_th_custom">m/Cuộn</th>
+          <th class="align-middle material_th_custom">m/Cây</th>
+
+          <th class="align-middle">Đ/v tính</th>
         <th class="align-middle">Lot No</th>
         <th class="align-middle">Số lượng (Theo chứng từ)</th>
         <th class="align-middle">Số lượng (Thực nhập)</th>
@@ -43,29 +54,66 @@
             <td class="sequence">{{ $index + 1 }}</td>
             <td class="code">
               <input type="hidden" name="product[merchandise_id][]" value="{{ $product['merchandise_id'] }}" />
-              <input class="form-control" style="width: 70px" type="text" name="product[code][]" value="{{ $product['code'] }}" readonly />
+              <input class="form-control" style="width: 70px" type="text" name="product[code][]" value="{{ $product['code'] ?? optional($product->merchandise)->code }}" readonly />
             </td>
             <td class="">
               <textarea class="form-control" style="width: 200px" name="product[name][]" rows="1" readonly>{{ $product['name'] }}</textarea>
             </td>
             <td class="do_day">
-              <input type="text" style="width: 70px" class="form-control" name="product[do_day][]" value="{{ $product['do_day'] }}"  readonly>
+              <input type="text" style="width: 70px" class="form-control" name="product[do_day][]" value="{{ $product['do_day'] ?? optional($product->merchandise)->do_day }}"  readonly>
             </td>
             <td class="hinh_dang">
-              <input type="text" style="width: 70px" class="form-control" name="product[hinh_dang][]" value="{{ $product['hinh_dang'] }}"  readonly>
+              <input type="text" style="width: 70px" class="form-control" name="product[hinh_dang][]" value="{{ $product['hinh_dang'] ?? optional($product->merchandise)->hinh_dang }}"  readonly>
             </td>
             <td class="dia_w_w1">
-              <input type="text" style="width: 70px" class="form-control" name="product[dia_w_w1][]" value="{{ $product['dia_w_w1'] }}"  readonly>
+              <input type="text" style="width: 70px" class="form-control" name="product[dia_w_w1][]" value="{{ $product['dia_w_w1'] ?? optional($product->merchandise)->dia_w_w1 }}"  readonly>
             </td>
             <td class="l_l1">
-              <input type="text" style="width: 70px" class="form-control" name="product[l_l1][]" value="{{ $product['l_l1'] }}"  readonly>
+              <input type="text" style="width: 70px" class="form-control" name="product[l_l1][]" value="{{ $product['l_l1'] ?? optional($product->merchandise)->l_l1 }}"  readonly>
             </td>
             <td class="w2">
-              <input type="text" style="width: 70px" class="form-control" name="product[w2][]" value="{{ $product['w2'] }}"  readonly>
+              <input type="text" style="width: 70px" class="form-control" name="product[w2][]" value="{{ $product['w2'] ?? optional($product->merchandise)->w2 }}"  readonly>
             </td>
             <td class="l2">
-              <input type="text" style="width: 70px" class="form-control" name="product[l2][]" value="{{ $product['l2'] }}"  readonly>
+              <input type="text" style="width: 70px" class="form-control" name="product[l2][]" value="{{ $product['l2'] ?? optional($product->merchandise)->l2 }}"  readonly>
             </td>
+
+              <td class="">
+                  <input class="form-control" type="text" name="product[inner][]" value="{{ optional($product->merchandise)->inner ?? null }}" readonly>
+              </td>
+              <td class="">
+                  <input class="form-control" type="text" name="product[hoop][]" value="{{ optional($product->merchandise)->hoop ?? null }}" readonly>
+              </td>
+              <td class="">
+                  <input class="form-control" type="text" name="product[filler][]" value="{{ optional($product->merchandise)->filler ?? null }}" readonly>
+              </td>
+              <td class="">
+                  <input class="form-control" type="text" name="product[outer][]" value="{{ optional($product->merchandise)->outer ?? null }}" readonly>
+              </td>
+              <td class="">
+                  <input class="form-control" type="text" name="product[thick][]" value="{{ optional($product->merchandise)->thick ?? null }}" readonly>
+              </td>
+              <td class="">
+                  <input class="form-control" type="text" name="product[tieu_chuan][]" value="{{ optional($product->merchandise)->tieu_chuan ?? null }}" readonly>
+              </td>
+              <td class="">
+                  <input class="form-control" type="text" name="product[kich_co][]" value="{{ optional($product->merchandise)->kich_co ?? null }}" readonly>
+              </td>
+              <td class="">
+                  <input class="form-control" type="text" name="product[trong_luong_cuon][]" value="{{ optional($product->merchandise)->trong_luong_cuon ?? null }}" readonly>
+              </td>
+              <td class="">
+                  <input class="form-control" type="text" name="product[m_cuon][]" value="{{ optional($product->merchandise)->m_cuon ?? null }}" readonly>
+              </td>
+              <td class="">
+                  <input class="form-control" type="text" name="product[m_cay][]" value="{{ optional($product->merchandise)->m_cay ?? null }}" readonly>
+              </td>
+              <td class=" d-none">
+                  <input class="form-control" type="text" name="product[kich_thuoc][]" value="{{ optional($product->merchandise)->kich_thuoc ?? null }}" readonly>
+              </td>
+              <td class="d-none">
+                  <input class="form-control" type="text" name="product[quy_cach][]" value="{{ $product['quy_cach'] ?? null }}" readonly>
+              </td>
             <td class="">
               <input class="form-control" style="width: 70px" type="text" name="product[unit][]" value="{{ $product['unit'] }}" readonly>
             </td>
