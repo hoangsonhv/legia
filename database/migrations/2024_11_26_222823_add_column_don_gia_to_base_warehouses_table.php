e3f2c16d6b09e7b22c00cfa27f60026d09fc82d5
Hoang Van Son <sonhv@bekisoft.com>
2024-11-27 10:20:32 +0700

    Fix name migration

--- database/migrations/2024_11_26_222823_add_column_don_gia_to_base_warehouses_table.php
+++ database/migrations/2024_11_26_222823_add_column_don_gia_to_base_warehouses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnDonGiaToMultipleTable extends Migration
+class AddColumnDonGiaToBaseWarehousesTable extends Migration
 {
     /**
      * Run the migrations.
